af3e0447bb428de37f347faee6be665cef321a73
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-06-24 19:25:55 +0200

    Adding details over the add method and adding a get method in the media type controller. Plus, enabling the return http not acceptable in the startup.

--- asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebApi.Model;
 
 namespace WebApi.Controllers
@@ -14,9 +15,23 @@ namespace WebApi.Controllers
     {
         [Route("addmoviecontenttypeexample")]
         [HttpPost]
-        public IActionResult AddMovieContentTypeExample(Movie movie)
+        public IActionResult AddMovieContentTypeExample([FromBody] Movie movie)
         {
-            throw new NotImplementedException();
+            return Ok(movie);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var movie = new Movie
+            {
+                Id = 2,
+                Name = "The dark knight",
+                Director = "Christopher Nolan",
+                Year = 2008
+            };
+
+            return Ok(movie);
         }
     }
 }
