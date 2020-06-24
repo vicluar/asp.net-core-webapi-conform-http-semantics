5debbce1288f1d4e82c85869634cd31e5fabda0f
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-06-25 00:27:53 +0200

    Adding a post methods controller for post samples. Plus, adding the create action.

---
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/PostMethodsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostMethodsController : ControllerBase
+    {
+        [Route("createmovie")]
+        [HttpPost]
+        public IActionResult CreateMovie(Movie movie)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = movie.Id }, movie);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var movie = new Movie
+            {
+                Id = 2,
+                Name = "The dark knight",
+                Director = "Christopher Nolan",
+                Year = 2008
+            };
+
+            return Ok(movie);
+        }
+    }
+}
