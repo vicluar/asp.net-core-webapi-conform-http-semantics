239462e9b80395cbd257edf8d4f994fca463590f
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-06-24 17:52:12 +0200

    Adding a model class for the sample project. Plus, adding a method for content type header sample.

--- asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
 
 namespace WebApi.Controllers
 {
@@ -11,5 +12,10 @@ namespace WebApi.Controllers
     [ApiController]
     public class MediaTypeController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult AddMovieContentTypeExample(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Model/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+    }
+}
