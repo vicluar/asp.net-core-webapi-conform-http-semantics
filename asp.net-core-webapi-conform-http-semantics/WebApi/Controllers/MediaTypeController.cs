c48b949ff3b00f84c11f6c727dee380a2b4a15dd
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-06-24 19:27:25 +0200

    Removing unused using.

--- asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/MediaTypeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebApi.Model;
 
 namespace WebApi.Controllers
