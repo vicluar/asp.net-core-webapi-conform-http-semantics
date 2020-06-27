742f59224069de1793adde1f2bdbe240e43d1063
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-06-27 07:50:26 +0200

    Adding the sample methods for the HTTP put requests.

--- asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/PutMethodsController.cs
+++ asp.net-core-webapi-conform-http-semantics/WebApi/Controllers/PutMethodsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Model;
 
 namespace WebApi.Controllers
 {
@@ -11,5 +12,46 @@ namespace WebApi.Controllers
     [ApiController]
     public class PutMethodsController : ControllerBase
     {
+        [Route("putcreatemovie")]
+        [HttpPut]
+        public IActionResult CreateMovie(Movie movie)
+        {
+            return CreatedAtAction(nameof(GetPutMovieById), new { id = movie.Id }, movie);
+        }
+
+        [Route("putupdatemovie")]
+        [HttpPut]
+        public IActionResult UpdateMovie(Movie movie)
+        {
+            return Ok(new { result = "Successful" });
+        }
+
+        [Route("putupdatemovienoresult")]
+        [HttpPut]
+        public IActionResult UpdateMovieNoResult(Movie movie)
+        {
+            return new NoContentResult();
+        }
+
+        [Route("notpossibleupdatedueconflict")]
+        [HttpPut]
+        public IActionResult NotPossibleUpdateDueConflict(Movie movie)
+        {
+            return Conflict();
+        }
+
+        [HttpGet]
+        public IActionResult GetPutMovieById(int id)
+        {
+            var movie = new Movie
+            {
+                Id = 2,
+                Name = "The dark knight",
+                Director = "Christopher Nolan",
+                Year = 2008
+            };
+
+            return Ok(movie);
+        }
     }
 }
